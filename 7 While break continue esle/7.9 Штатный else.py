e54778fd600352e53ae138f18ce15aa44aa7caf5
nikita <nikita.chudnovskiy@mail.ru>
2025-01-30 17:13:46 +0300

    message

--- 7 While break continue esle/7.9 Штатный else.py
+++ 7 While break continue esle/7.9 Штатный else.py
@@ -2,16 +2,15 @@ from logging import getHandlerByName
 
 n = 0
 
-while n<5:
-    n+=1
-    if n ==4:
+while n < 5:
+    n += 1
+    if n == 4:
         break
     print(n)
 else:
     print('Цикл завершен')
 
 print()
-
 
 # Напишем 2 кода
 num = 666
@@ -21,7 +20,7 @@ while n != 0:
     last = n % 10
     if last == 7:
         flag = True
-        break        # прерываем цикл, так как число гарантированно содержит цифру 7
+        break  # прерываем цикл, так как число гарантированно содержит цифру 7
     n //= 10
 
 if flag == True:
@@ -31,11 +30,17 @@ else:
 
 print()
 n = 757
-while n!=0:
-    l=n%10
-    if l ==7:
-        print('Содержит', n, 'число',l)
+while n != 0:
+    l = n % 10
+    if l == 7:
+        print('Содержит', n, 'число', l)
         break
-    n=n//10
+    n = n // 10
 else:
     print('Не содержит 7')
+
+n = 0
+while n > 0:
+    print(n)
+else:
+    print('Цунгванг˚')
