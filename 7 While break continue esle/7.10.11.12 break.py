c566bbac5e825a9f3de0c4ded9d0fc327f5f965e
nikita <nikita.chudnovskiy@mail.ru>
2025-01-31 10:59:58 +0300

    message

---
+++ 7 While break continue esle/7.10.11.12 break.py
@@ -0,0 +1,26 @@
+n = 0
+while n < 10:
+    n += 2
+    print(n)
+else:
+    print('Конец цикла')
+
+n = 0
+while n < 10:
+    n += 2
+    print(n)
+    if n == 8:
+        break
+else:
+    print('Конец цикла')
+
+
+n = 0
+while n < 10:
+    n += 2
+    print(n)
+    if n == 7:
+        break
+else:
+    print('Конец цикла')
+
